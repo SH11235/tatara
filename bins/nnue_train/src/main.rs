df2ea17249be503dff66349f9d4c2a852d1392bb
SH11235 <aabceh1a1a2i3u5o8@gmail.com>
2026-05-15 03:59:58 +0900

    Revert "nnue_train: 候補 ε prof_tick で per-launch optimizer 内訳計測 (NO-GO 0.18%)"
    This reverts commit c0758820f8864d9095137e09e5d89f460e45b28c.

--- bins/nnue_train/src/main.rs
+++ bins/nnue_train/src/main.rs
@@ -4873,7 +4873,6 @@ impl GpuTrainer {
                    slice_mut(self.ft_w_grad), lr, step_size, denom, DECAY, BETA1, BETA2, EPS,
                    MIN_W, MAX_W, ft_w_n as u32]
         }?;
-        prof_tick!("opt_ft_w");
         cuda_launch! {
             kernel: radam_step,
             stream: self.stream, module: self.module, config: cfg_1d(ft_b_n),
@@ -4881,7 +4880,6 @@ impl GpuTrainer {
                    slice_mut(self.ft_b_grad), lr, step_size, denom, DECAY, BETA1, BETA2, EPS,
                    MIN_W, MAX_W, ft_b_n as u32]
         }?;
-        prof_tick!("opt_ft_b");
         // L1
         cuda_launch! {
             kernel: radam_step,
@@ -4890,7 +4888,6 @@ impl GpuTrainer {
                    slice_mut(self.l1_w_grad), lr, step_size, denom, DECAY, BETA1, BETA2, EPS,
                    MIN_W, MAX_W, l1_w_n as u32]
         }?;
-        prof_tick!("opt_l1_w");
         cuda_launch! {
             kernel: radam_step,
             stream: self.stream, module: self.module, config: cfg_1d(l1_b_n),
@@ -4898,7 +4895,6 @@ impl GpuTrainer {
                    slice_mut(self.l1_b_grad), lr, step_size, denom, DECAY, BETA1, BETA2, EPS,
                    MIN_W, MAX_W, l1_b_n as u32]
         }?;
-        prof_tick!("opt_l1_b");
         // L1f
         cuda_launch! {
             kernel: radam_step,
@@ -4907,7 +4903,6 @@ impl GpuTrainer {
                    slice_mut(self.l1f_w_grad), lr, step_size, denom, DECAY, BETA1, BETA2, EPS,
                    MIN_W, MAX_W, l1f_w_n as u32]
         }?;
-        prof_tick!("opt_l1f_w");
         cuda_launch! {
             kernel: radam_step,
             stream: self.stream, module: self.module, config: cfg_1d(l1f_b_n),
@@ -4915,7 +4910,6 @@ impl GpuTrainer {
                    slice_mut(self.l1f_b_grad), lr, step_size, denom, DECAY, BETA1, BETA2, EPS,
                    MIN_W, MAX_W, l1f_b_n as u32]
         }?;
-        prof_tick!("opt_l1f_b");
         // L2
         cuda_launch! {
             kernel: radam_step,
@@ -4924,7 +4918,6 @@ impl GpuTrainer {
                    slice_mut(self.l2_w_grad), lr, step_size, denom, DECAY, BETA1, BETA2, EPS,
                    MIN_W, MAX_W, l2_w_n as u32]
         }?;
-        prof_tick!("opt_l2_w");
         cuda_launch! {
             kernel: radam_step,
             stream: self.stream, module: self.module, config: cfg_1d(l2_b_n),
@@ -4932,7 +4925,6 @@ impl GpuTrainer {
                    slice_mut(self.l2_b_grad), lr, step_size, denom, DECAY, BETA1, BETA2, EPS,
                    MIN_W, MAX_W, l2_b_n as u32]
         }?;
-        prof_tick!("opt_l2_b");
         // L3
         cuda_launch! {
             kernel: radam_step,
@@ -4941,7 +4933,6 @@ impl GpuTrainer {
                    slice_mut(self.l3_w_grad), lr, step_size, denom, DECAY, BETA1, BETA2, EPS,
                    MIN_W, MAX_W, l3_w_n as u32]
         }?;
-        prof_tick!("opt_l3_w");
         cuda_launch! {
             kernel: radam_step,
             stream: self.stream, module: self.module, config: cfg_1d(l3_b_n),
@@ -4949,7 +4940,6 @@ impl GpuTrainer {
                    slice_mut(self.l3_b_grad), lr, step_size, denom, DECAY, BETA1, BETA2, EPS,
                    MIN_W, MAX_W, l3_b_n as u32]
         }?;
-        prof_tick!("opt_l3_b");
 
         // Lookahead lerp every K steps
         if self.step_count.is_multiple_of(RANGER_K) {
